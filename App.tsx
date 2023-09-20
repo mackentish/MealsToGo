329432a1c7756ea97d7d645ef17ebeb42e147285
Brandon Trzepacz Tish <46979741+mackentish@users.noreply.github.com>
2023-09-20 15:01:02 -0400

    search bar and statusbar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   SafeAreaView,
   StyleSheet,
@@ -6,31 +6,61 @@ import {
   View,
   Platform,
   StatusBar,
+  Appearance,
 } from "react-native";
+import { PaperProvider, DefaultTheme, Searchbar } from "react-native-paper";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+
+const colorScheme = Appearance.getColorScheme();
+const theme = {
+  ...DefaultTheme,
+  dark: colorScheme === "dark",
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#AF5B5B",
+    secondary: "#92CCFF",
+    tertiary: "#ECDD7B",
+  },
+};
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.search}>
-        <Text>Search</Text>
-      </View>
+  const [searchQuery, setSearchQuery] = useState("");
 
-      <View style={styles.list}>
-        <Text>List</Text>
-      </View>
-    </SafeAreaView>
+  return (
+    <PaperProvider theme={theme}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Searchbar
+            value={searchQuery}
+            onChangeText={setSearchQuery}
+            placeholder="Search"
+            style={styles.searchBar}
+            icon="heart-outline"
+          />
+        </View>
+
+        <View style={styles.list}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style={colorScheme === "dark" ? "light" : "dark"} />
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    backgroundColor: colorScheme === "dark" ? "black" : "white",
   },
   search: {
     padding: 16,
     flexDirection: "row",
     backgroundColor: "green",
+  },
+  searchBar: {
+    flex: 1,
   },
   list: {
     padding: 16,
