431743d0aa6af71361e6ca422bed5ef91b0fbcd0
Brandon Trzepacz Tish <46979741+mackentish@users.noreply.github.com>
2023-09-20 13:44:58 -0400

    add eslint

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        <Text>Search</Text>
+      </View>
+
+      <View style={styles.list}>
+        <Text>List</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  search: {
+    padding: 16,
+    flexDirection: "row",
+    backgroundColor: "green",
+  },
+  list: {
+    padding: 16,
+    flex: 1,
+    backgroundColor: "blue",
   },
 });
