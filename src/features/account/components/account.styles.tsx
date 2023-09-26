d19372c02ef9312c2c4dbca9a74e2af79f7a5d25
Brandon Trzepacz Tish <46979741+mackentish@users.noreply.github.com>
2023-09-26 15:47:49 -0400

    animation

--- src/features/account/components/account.styles.tsx
+++ src/features/account/components/account.styles.tsx
@@ -32,3 +32,10 @@ export const AuthInput = styled(TextInput)`
 export const Title = styled(Text)`
   font-size: 30px;
 `;
+
+export const AnimationWrapper = styled.View`
+  width: 100%;
+  height: 40%;
+  position: absolute;
+  top: 30px;
+`;
--- src/features/account/screens/account.screen.tsx
+++ src/features/account/screens/account.screen.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import LottieView from "lottie-react-native";
 import { Button } from "react-native-paper";
 import {
   AccountBackground,
   AccountContainer,
   AccountOpacity,
   Title,
+  AnimationWrapper,
 } from "../components/account.styles";
 import colors from "../../../infrastructure/theme/colors";
 
@@ -12,6 +14,15 @@ export default function AccountScreen({ navigation }: any) {
   return (
     <AccountBackground source={{}}>
       <AccountOpacity />
+      <AnimationWrapper>
+        <LottieView
+          key="animation"
+          autoPlay
+          loop
+          resizeMode="cover"
+          source={require("../../../../assets/watermelon.json")}
+        />
+      </AnimationWrapper>
       <Title>Meals To Go</Title>
       <AccountContainer>
         <Button
